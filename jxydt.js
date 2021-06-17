451ab96cd7e115d5b5715056ab03118c4a9785ef
丶小夜愿 <62316946+smiles-seaside@users.noreply.github.com>
2021-06-17 11:25:06 +0800

    Update jxydt.js

--- jxydt.js
+++ jxydt.js
@@ -12,7 +12,97 @@ var obj = JSON.parse($response.body);
       "deviceOverflow": "0",
       "status": "5",
       "overdueSecond": "-1"
+    },
+    "RELIABLE_VIP_KM4": {
+      "resourceId": "",
+      "residualSecond": "0",
+      "rightsType": "RELIABLE_VIP_KM4",
+      "description": null,
+      "beginTime": "2021-06-16",
+      "endTime": "2021-06-16",
+      "deviceOverflow": "0",
+      "status": "5",
+      "overdueSecond": "0"
+    },
+    "RELIABLE_VIP_KM1": {
+      "resourceId": "",
+      "residualSecond": "0",
+      "rightsType": "RELIABLE_VIP_KM1",
+      "description": null,
+      "beginTime": "2021-06-16",
+      "endTime": "2021-06-16",
+      "deviceOverflow": "0",
+      "status": "5",
+      "overdueSecond": "0"
+    },
+    "SVIP_LIVECAMP": {
+      "resourceId": "",
+      "residualSecond": "0",
+      "rightsType": "SVIP_LIVECAMP",
+      "description": null,
+      "beginTime": "2021-06-16",
+      "endTime": "2021-06-16",
+      "deviceOverflow": "0",
+      "status": "5",
+      "overdueSecond": "0"
+    },
+    "VIP": {
+      "resourceId": "",
+      "residualSecond": "0",
+      "rightsType": "VIP",
+      "description": null,
+      "beginTime": "2021-06-16",
+      "endTime": "2021-06-16",
+      "deviceOverflow": "0",
+      "status": "5",
+      "overdueSecond": "0"
+    },
+    "SVIP": {
+      "resourceId": "",
+      "residualSecond": "0",
+      "rightsType": "SVIP",
+      "description": null,
+      "beginTime": "2021-06-16",
+      "endTime": "2021-06-16",
+      "deviceOverflow": "0",
+      "status": "5",
+      "overdueSecond": "0"
+    },
+    "SPEED_VIP_KM1": {
+      "resourceId": "",
+      "residualSecond": "0",
+      "rightsType": "SPEED_VIP_KM1",
+      "description": null,
+      "beginTime": "2021-06-16",
+      "endTime": "2021-06-16",
+      "deviceOverflow": "0",
+      "status": "5",
+      "overdueSecond": "0"
+    },
+    "SPEED_VIP_KM2": {
+      "resourceId": "",
+      "residualSecond": "0",
+      "rightsType": "SPEED_VIP_KM2",
+      "description": null,
+      "beginTime": "2021-06-16",
+      "endTime": "2021-06-16",
+      "deviceOverflow": "0",
+      "status": "5",
+      "overdueSecond": "0"
+    },
+    "SPEED_VIP_KM3": {
+      "resourceId": "",
+      "residualSecond": "0",
+      "rightsType": "SPEED_VIP_KM3",
+      "description": null,
+      "beginTime": "2021-06-16",
+      "endTime": "2021-06-16",
+      "deviceOverflow": "0",
+      "status": "5",
+      "overdueSecond": "0"
     }
+  },
+  "msg": "ok"
 };
 $done({body: JSON.stringify(obj)});
 //
